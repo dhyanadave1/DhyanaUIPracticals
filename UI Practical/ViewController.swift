7ecb97f2d1e91071a77622a720ed8f8f25309128
Dhyana Dave <dhyana.d@simformsolutions.com>
2022-04-29 19:56:12 +0530

    Init commit

---
+++ UI Practical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Practical
+//
+//  Created by Dhyana Dave on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
